b48c0475159dfca508fb068706ec9828c7ba3b64
s4t0masami <sadbuenocat@gmail.com>
2024-11-14 12:14:38 -0300

    Atualização Informações e Routes

--- app/views.py
+++ app/views.py
@@ -1,5 +1,16 @@
 from app import app
-from flask import render_template
+from flask import render_template, url_for
 @app.route('/')
 def homepage():
-    return render_template('index.html', message='estou conectado ao back-end!')
+    
+    idade = 25
+    usuario = 'Arthon Lambda'
+    context = {
+        'idade':idade,
+        'usuario':usuario
+    }
+    return render_template('index.html', message='estou conectado ao back-end!',context=context)
+
+@app.route('/news/')
+def newpage():
+    return 'Nova página'
