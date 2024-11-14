523d65e3b97d77b6c2e90134f132052376d2ebf6
s4t0masami <sadbuenocat@gmail.com>
2024-11-13 22:36:30 -0300

    primeira atualização

--- app/views.py
+++ app/views.py
@@ -2,4 +2,4 @@ from app import app
 from flask import render_template
 @app.route('/')
 def homepage():
-    return render_template('index.html')
+    return render_template('index.html', message='estou conectado ao back-end!')
